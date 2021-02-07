a5ab5b3c1b6b9254ea69429c75df148033646e87
sntptrck <sntptrck@gmail.com>
2021-02-07 18:49:43 +0300

    singleton

--- patterns/Singleton/index.ts
+++ patterns/Singleton/index.ts
@@ -1,5 +1,5 @@
 export class Singleton {
-  static instance?: Singleton;
+  private static instance?: Singleton;
   data: unknown;
   constructor(data: unknown) {
     if (typeof Singleton.instance === "object") {
@@ -7,6 +7,10 @@ export class Singleton {
     }
     this.data = data;
     Singleton.instance = this;
+    return this;
+  }
+
+  getInstance(): Singleton {
     return this;
   }
 }
